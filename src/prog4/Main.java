11dd1095f9eb4508c7c86960d8716b94ade944c3
Kai <40851151+Kheang1409@users.noreply.github.com>
2024-06-02 20:42:07 -0500

    Update Main.java

--- src/prog4/Main.java
+++ src/prog4/Main.java
@@ -21,11 +21,11 @@ public class Main {
 
         Employee[] employees = new Employee[5];
 
-        employees[0] = new CommissionEmployee("John", "Smith", "123-456-789", 2000, 0.10);
+        employees[0] = new CommissionEmployee("John", "Smith", "123-456-789", 2000, 10);
         employees[1] = new HourlyEmployee("Alexander", "John", "678-678-213", 120, 45.6);
         employees[2] = new SalariedEmployee("Jennifer", "Lopez", "123-456-789", 1230);
-        employees[3] = new BasePlusCommissionEmployee("Bruce", "Lee", "5678-321-456", 4560, 0.07, 789);
-        employees[4] = null; //Test NPE
+        employees[3] = new BasePlusCommissionEmployee("Bruce", "Lee", "5678-321-456", 4560, 7, 789);
+        //employees[4] = null; Test NPE
 
         System.out.println(findMaxSalary(employees));
     }
