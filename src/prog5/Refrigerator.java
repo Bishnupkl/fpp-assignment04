0707cfa95edf29fc782dcdde978690acbe629b16
bishnu <bishnup212@gmail.com>
2024-06-03 14:18:31 -0500

    problem 5 solved

--- src/prog5/Refrigerator.java
+++ src/prog5/Refrigerator.java
@@ -1,13 +1,19 @@
 package prog5;
 
-public class Refrigerator  {
+public class Refrigerator implements HomeService {
     private int temperature;
+
     public Refrigerator(int temperature) {
+
         this.temperature = temperature;
+
     }
+
     public int getTemperature() {
         return temperature;
     }
+
+    @Override
     public void performFunction() {
         System.out.println("Cooling items at temperature: " + temperature + "°C.");
     }
--- src/prog5/WashingMachine.java
+++ src/prog5/WashingMachine.java
@@ -1,6 +1,6 @@
 package prog5;
 
-public class WashingMachine  {
+public class WashingMachine implements HomeService {
     private int loadCapacity;
 
     public WashingMachine(int loadCapacity) {
@@ -8,9 +8,12 @@ public class WashingMachine  {
     }
 
     public int getLoadCapacity() {
+
         return loadCapacity;
+
     }
 
+    @Override
     public void performFunction() {
         System.out.println("Starting wash cycle with load capacity: " + loadCapacity + " kg.");
     }
